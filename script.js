cb8aa91a4c7ab181136fc6b06d817e52c798dbfd
metehanbati <mthnbt@gmail.com>
2025-03-13 03:00:48 +0300

    Added last week's files to week3 branch

--- script.js
+++ script.js
@@ -1,37 +1,12 @@
-const loginAttempts = [];
-let formsHidden = false;
-
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    loginAttempts.push({ username, password });
-    console.log(loginAttempts);
-
-    if (username === 'Ash' && password === 'Pikachu') {
-        alert('Giriş başarılı!');
+    if (username === 'admin' && password === 'admin') {
+        window.location.href = 'table.html'; // Giriş başarılıysa table.html'ye yönlendir
     } else {
         alert('Kullanıcı adı veya şifre hatalı!');
-    }
-});
-
-function updateClock() {
-    const now = new Date();
-    const timeString = now.toLocaleTimeString();
-    document.getElementById('live-clock').textContent = timeString;
-}
-
-updateClock();
-setInterval(updateClock, 1000);
-
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'h' || event.key === 'H') {
-        formsHidden = !formsHidden;
-        const forms = document.querySelectorAll('form');
-        forms.forEach(form => {
-            form.style.display = formsHidden ? 'none' : 'block';
-        });
     }
 });
